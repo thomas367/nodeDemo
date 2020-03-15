5ecee5f8098bfc643f11c906a844172b6d0c8ee6
thomas <thomas.367@hotmail.com>
2020-03-15 13:37:28 +0200

    create auth middleware

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const app = express();
 
 connectDB();
 
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 //Access user route
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
