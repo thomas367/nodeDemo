92f6b4ae74e8b990e7dfb22a5b80199f4a837ff2
thomas <thomas.367@hotmail.com>
2020-03-15 16:17:12 +0200

    change routes structure, move logic to controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const router =  express.Router();
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
-const gravatar = require('gravatar');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config();
 
-const User = require('../../models/User');
+const User = require('../models/User');
 
-/**
- *  @route      POST api/users
- *  @desc       Register user
- */
-router.post('/', [
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-], async (req, res) => {
+exports.register = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -64,8 +54,46 @@ router.post('/', [
         console.log(err.message);
         res.status(500).send('Server error');
     }
+};
 
-    
-});
+exports.login = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports = router;
+    const {email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ errors: [{ message: 'Invalid Credentials' }] });
+        }
+
+        const isMatched = await bcrypt.compare(password, user.password);
+
+        if (!isMatched) {
+            return res.status(400).json({ errors: [{ message: 'Invalid Credentials' }] });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            process.env.jwtSecret,
+            {expiresIn: 36000},
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+        });
+
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send('Server error');
+    }
+};
---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router =  express.Router();
+const authCtrl = require('../controllers/authController');
+
+router.post('/login', authCtrl.login);
+router.post('/register', authCtrl.register);
+
+module.exports = router;
