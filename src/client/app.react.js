0547de3908cc87dbf1fbe884e25830a633004642
thomas <thomas.367@hotmail.com>
2020-05-09 14:17:21 +0300

    clean up app.scss

--- src/client/app.react.js
+++ src/client/app.react.js
@@ -4,22 +4,22 @@ import Landing from 'client/components/layout/landing/landing.react';
 import Login from 'client/components/auth/login/login.react';
 import Register from 'client/components/auth/register/register.react';
 import Dashboard from 'client/components/dashboard/dashboard.react';
+import CreateProfile from 'client/components/profile/createProfile/createProfile.react';
 import PrivateRoute from 'client/components/routing/privateRoute.react';
 import { Route, Switch } from 'react-router-dom';
-import styles from 'client/app.scss';
+import 'client/app.scss';
 
 function App() {
     return (
         <>
             <Navbar />
             <Route exact path="/" component={Landing} />
-            <section className={styles.container}>
-                <Switch>
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/login" component={Login} />
-                    <PrivateRoute exact path="/dashboard" component={Dashboard} />
-                </Switch>
-            </section>
+            <Switch>
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute exact path="/createProfile" component={CreateProfile} />
+            </Switch>
         </>
     );
 }
