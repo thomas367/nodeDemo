235e0d88214b7bc34ff971db4196e03c0e113521
thomas <thomas.367@hotmail.com>
2020-05-17 14:14:05 +0300

    create production webpack and ci script for deploy

---
+++ webpack.config.prod.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.react.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        modules: [
+            path.resolve(process.cwd(), 'src'),
+            path.resolve(process.cwd(), 'node_modules'),
+            path.resolve(process.cwd(), 'assets')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.[s]css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]--[hash:base64:5]'
+                            },
+                            sourceMap: false
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.png|jpg$/,
+                include: path.join(__dirname, 'assets/images'),
+                loader: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['dist']
+        }),
+        new UglifyJSPlugin()
+    ]
+};
---
+++ webpack.config.styles.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const glob = require('glob');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: glob.sync(path.resolve(process.cwd(), 'src/client/**/*.scss')),
+    output: {
+        path: path.join(__dirname, 'dist', 'css'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        modules: [
+            path.resolve(process.cwd(), 'src'),
+            path.resolve(process.cwd(), 'node_modules'),
+            path.resolve(process.cwd(), 'assets')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[s]css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[hash:base64:5]'
+                            },
+                            sourceMap: false
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.png|jpg$/,
+                include: path.join(__dirname, 'assets/images'),
+                loader: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'dist', 'css')],
+            cleanAfterEveryBuildPatterns: [path.join(__dirname, 'dist', 'css', 'bundle.js')]
+        }),
+        new UglifyJSPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
+    ]
+};
