be8eab2ce6616fc4ec4856e9b045713147f06073
thomas <thomas.367@hotmail.com>
2020-05-30 12:56:46 +0300

    test undefined method

--- src/server/server.js
+++ src/server/server.js
@@ -14,7 +14,7 @@ app.use('/api', routes);
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', res => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
